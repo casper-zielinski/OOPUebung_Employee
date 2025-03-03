3ca23bd1a323ac3239e6a9a2a81f3c8906b091f3
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-03-03 22:46:48 +0100

    setandconst

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 1. initial commit, added java classes, gitignore and pushed repository.
 
 2. gettermethods, created getter methods for the member variables.
+
+3. setandconst, settermethods and 2 constructors with an id that increase each time a new object is created 
--- src/at/fhj/msd/Employee.java
+++ src/at/fhj/msd/Employee.java
@@ -2,10 +2,12 @@ package at.fhj.msd;
 
 public class Employee {
 
-      private String firstName = "lol";
-      private String lastName = "sos";
-      private int id = 3;
-      private double salary = 22.3;
+      private String firstName;
+      private String lastName;
+      private static int currentId = 1;
+      private int id;
+      private double salary;
+      
 
       public int getId()
       {
@@ -19,9 +21,69 @@ public class Employee {
 
       public String getName () 
       {
-          return lastName.toUpperCase() + firstName;
+          return lastName.toUpperCase() + " " + firstName;
       }
 
+      public void setfirstName (String firstName)
+      {
+            if (firstName == null)
+            {
+                  throw new IllegalArgumentException(" Name should not be null, include a name please :) ");
+            }
+            else 
+            {
+                  this.firstName = firstName;
+            }
+            
+      }
 
+      public void setLastName (String lastName)
+      {
+            if (lastName == null)
+            {
+                  throw new IllegalArgumentException(" Name should not be null, include a name please :) ");
+            }
+            else 
+            {
+                  this.lastName = lastName;
+            }
+      }
+
+      public void setsalary (double salary)
+      {
+            if (salary >= 0)
+            {
+                  this.salary = salary;
+            }
+            else if (salary < 0)
+            {
+                  throw new IllegalArgumentException("Salary shouldn't be in the negatives");
+            }
+            else
+            {
+                  throw new IllegalArgumentException("Error 404");
+            }
+
+            
+      }
+
+      public Employee (String firstname, String lastname, double salary)
+      {
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.salary = salary;
+            this.id = currentId;
+            currentId++;
+      }
+
+      public Employee (String firstname, String lastname)
+      {
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.id = currentId;
+            currentId++;
+      }
+
+      
 
 }
